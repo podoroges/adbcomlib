6216b897dbad74921b070ce43e4725426f8d0c31
podoroges <pavel.krents@gmail.com>
2016-11-30 13:48:17 +0200

    Update adbstrings.cpp

--- adbstrings.cpp
+++ adbstrings.cpp
@@ -19,128 +19,95 @@ AnsiString StrRep(AnsiString st,AnsiString from,AnsiString to){
   return st;
 }
 //---------------------------------------------------------------------------
-// Конвертируем 4-символьные 6-битные в 3-символьные 8-битные группы
-// Большая часть кода позаимствована отсюда:
-// http://base64.sourceforge.net/b64.c
-// DecodeBase64("RndkOiDQ0snXxdTJy8kpKQ==") = "Fwd: приветики))"
-AnsiString DecodeBase64(AnsiString st)
-{
-  const char cd64[]="|$$$}rstuvwxyz{$$$$$$$>?@ABCDEFGHIJKLMNOPQRSTUVW$$$$$$XYZ[\\]^_`abcdefghijklmnopq";
-	                  
-  AnsiString st2;
-  unsigned char in[4],out[3],v;
-  int i,len,j = 0;
-  while(j<st.Length()){
-    for(len=0,i=0;i<4&&(j<st.Length());i++){
-      v=0;
-      while((j<st.Length())&&(v==0)){
-        v=(unsigned char)st[++j];
-        v=(unsigned char)((v<43||v>122)?0:cd64[v-43]);
-        if(v)
-          v=(unsigned char)((v=='$')?0:v-61);
-      }
-      if(j<st.Length()){
-        len++;
-        if(v)
-          in[i]=(unsigned char)(v-1);
-      }
-      else{
-        in[i]=0;
+// Base64
+// Source: http://fm4dd.com/programming/base64/base64_stringencode_c.htm
+char b64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+/* decodeblock - decode 4 '6-bit' characters into 3 8-bit binary bytes */
+AnsiString decodeblock(unsigned char in[]) {
+  unsigned char out[4];
+  out[0] = in[0] << 2 | in[1] >> 4;
+  out[1] = in[1] << 4 | in[2] >> 2;
+  out[2] = in[2] << 6 | in[3] >> 0;
+  out[3] = '\0';
+  return AnsiString((char *)out);
+}
+AnsiString DecodeBase64(AnsiString src) {
+  AnsiString st;
+  char *b64src = src.c_str();
+  int c, phase, i;
+  unsigned char in[4];
+  char *p;
+
+  phase = 0; i=0;
+  while(b64src[i]) {
+    c = (int) b64src[i];
+    if(c == '=') {
+      st = (AnsiString)st+decodeblock(in);
+      break;
+    }
+    p = strchr(b64, c);
+    if(p) {
+      in[phase] = p - b64;
+      phase = (phase + 1) % 4;
+      if(phase == 0) {
+        st = (AnsiString)st+decodeblock(in);
+        in[0]=in[1]=in[2]=in[3]=0;
       }
     }
-    if(len){
-      out[0]=(unsigned char)(in[0]<<2|in[1]>>4);
-      out[1]=(unsigned char)(in[1]<<4|in[2]>>2);
-      out[2]=(unsigned char)(((in[2]<<6)&0xc0)|in[3]);
-      for(i=0;i<len-1;i++)
-        st2 = st2+(char)out[i];
+    i++;
+  }
+  return st;
+}
+/* encodeblock - encode 3 8-bit binary bytes as 4 '6-bit' characters */
+AnsiString encodeblock( unsigned char in[], int len ) {
+    unsigned char out[5];
+    out[0] = b64[ in[0] >> 2 ];
+    out[1] = b64[ ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4) ];
+    out[2] = (unsigned char) (len > 1 ? b64[ ((in[1] & 0x0f) << 2) |
+             ((in[2] & 0xc0) >> 6) ] : '=');
+    out[3] = (unsigned char) (len > 2 ? b64[ in[2] & 0x3f ] : '=');
+    out[4] = '\0';
+    return AnsiString((char *)out);
+}
+/* encode - base64 encode a stream, adding padding if needed */
+AnsiString b64_encode(char *clrstr, int cnt) {
+  AnsiString st;
+  unsigned char in[3];
+  int i, len = 0;
+  int j = 0;
+
+  while(j<cnt) {
+    len = 0;
+    for(i=0; i<3; i++) {
+     in[i] = (unsigned char) clrstr[j];
+     if(clrstr[j]) {
+        len++; j++;
+      }
+      else in[i] = 0;
+    }
+    if( len ) {
+      st = (AnsiString) st+encodeblock( in, len );
     }
   }
-  return st2;
+
+  return st;
 }
 
 AnsiString EncodeBase64(AnsiString st){
-  static const char cb64[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  AnsiString st2;
-  int i = 0;
-  int j = 0;
-  unsigned char char_array_3[3];
-  unsigned char char_array_4[4];
-  int k = 0;
-
-  while (++k<=st.Length()) {
-    char_array_3[i++] = st[k];
-    if (i == 3) {
-      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-      char_array_4[3] = char_array_3[2] & 0x3f;
-
-      for(i = 0; (i <4) ; i++)
-        st2 = st2+cb64[char_array_4[i]];
-      i = 0;
-    }
-  }
-
-  if (i)
-  {
-    for(j = i; j < 3; j++)
-      char_array_3[j] = '\0';
-
-    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-    char_array_4[3] = char_array_3[2] & 0x3f;
-
-    for (j = 0; (j < i + 1); j++)
-      st2 = st2+cb64[char_array_4[j]];
-
-    while((i++ < 3))
-     st2 = st2+'=';
-
-  }
-
-  return st2;
+  return b64_encode(st.c_str(), st.Length());
 }
-
 
 AnsiString EncodeBase64(TStream * s){
-  static const char cb64[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  AnsiString st2;
-  int i = 0;
-  int j = 0;
-  unsigned char char_array_3[3];
-  unsigned char char_array_4[4];
-  s->Position = 0;
-  while (s->Position<s->Size) {
-    s->ReadBuffer(&char_array_3[i++] ,1);
-    if (i == 3) {
-      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-      char_array_4[3] = char_array_3[2] & 0x3f;
-      for(i = 0; (i <4) ; i++)
-        st2 = st2+cb64[char_array_4[i]];
-      i = 0;
-    }
-  }
-  if (i)
-  {
-    for(j = i; j < 3; j++)
-      char_array_3[j] = '\0';
-    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-    char_array_4[3] = char_array_3[2] & 0x3f;
-    for (j = 0; (j < i + 1); j++)
-      st2 = st2+cb64[char_array_4[j]];
-    while((i++ < 3))
-     st2 = st2+'=';
-  }
-  return st2;
+  int size = s->Size;
+  char * buf = new char[size+1];
+  s->ReadBuffer(buf,size);
+  AnsiString st = b64_encode(buf, size);
+  delete [] buf;
+  return st;
 }
 
-// Make base64 strings url friendly 
+
+// Make base64 strings url friendly
 // http://stackoverflow.com/questions/1374753/passing-base64-encoded-strings-in-url
 AnsiString UrlEncodeBase64(AnsiString st){
 	st = EncodeBase64(st);
